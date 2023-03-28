3ab34af0980b731f5c062e64def80a2d588741ec
shaumicwc <112300736+shaumicwc@users.noreply.github.com>
2023-03-29 00:11:54 +0600

    save cart to local storage

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,24 +6,26 @@ import './Cart.css'
 const Cart = ({ cart }) => {
 
     let totalPrice = 0;
-    let totalShipping =0;
-    for (const product of cart){
+    let totalShipping = 0;
+    for (const product of cart) {
         totalPrice = totalPrice + product.price;
         totalShipping = totalShipping + product.shipping;
     }
-    const tax = totalPrice*7/100;
+    const tax = totalPrice * 7 / 100;
 
     const grandTotal = totalPrice + totalShipping + tax;
 
 
     return (
         <div className='cart'>
-            <h4>Order Summary</h4>
-            <p>Selected Items:{cart.length}</p>
-            <p>Total Price: ${totalPrice}</p>
-            <p>Total Shipping Charge: ${totalShipping}</p>
-            <p>Tax: ${tax.toFixed(2)}</p>
-            <h6 className='grand-total'>Grand Total: ${grandTotal.toFixed(2)}</h6>
+            <div>
+                <h4 className='order-summary'>Order Summary</h4>
+                <p>Selected Items:{cart.length}</p>
+                <p>Total Price: ${totalPrice}</p>
+                <p>Total Shipping Charge: ${totalShipping}</p>
+                <p>Tax: ${tax.toFixed(2)}</p>
+                <h6 className='grand-total'>Grand Total: ${grandTotal.toFixed(2)}</h6>
+            </div>
         </div>
     );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product';
 import './Shop.css';
+import Cart from '../Cart/Cart';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 
 
 const Shop = () => {
@@ -14,9 +16,15 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, []);
 
+    useEffect ( ()=>{
+        const storedCart =getShoppingCart();
+        console.log(storedCart);
+    },[])
+
     const handleAddToCart = (product) => {
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb
     }
 
 
